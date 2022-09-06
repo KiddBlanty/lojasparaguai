71d98b99aa25dcb6537b9f336e9159d6f93d457e
KiddBlanty <@mario-antonio_11@hotmail.com>
2022-09-06 17:33:08 -0400

    actualizacion de puerto

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors")
 const path = require("path")
 
 const app = express()
-const port = process.env.Port || 3000
+const port = process.env.PORT || 3000
 
 // Mis Importaciones
 const CategoriaModel = require("./model/CategoriaModel.class")
